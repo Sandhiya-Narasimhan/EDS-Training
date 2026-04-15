1719507eee67fae642102c5d9a482974a9c6360c
Sandhiya-Narasimhan <sandhiya.narasimhan@gmail.com>
2026-04-15 15:27:30 +0530

    Update widget.js

--- blocks/widget/widget.js
+++ blocks/widget/widget.js
@@ -1,15 +1,10 @@
 import icon from '../../icons/lock.png';
 
-const widgetH2 = document.querySelectorAll('.widget h3')
+const widgetH2 = document.querySelectorAll('.widget h3');
 widgetH2.forEach(ele => {
-
-// 2. Create the new image element
-var newImg = document.createElement('img');
-newImg.classList.add('widget-icon');
-newImg.src =  `${icon}`;
-newImg.alt = 'Description';
-
-// e. Insert the newImg before the sibling
-ele.parentNode.insertBefore(newImg, ele);
-
+  var newImg = document.createElement('img');
+  newImg.classList.add('widget-icon');
+  newImg.src =  `${icon}`;
+  newImg.alt = 'Description';
+  ele.parentNode.insertBefore(newImg, ele);
 })
