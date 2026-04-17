c72f161e79bac41a8329b39414d45829ff91acc2
Sandhiya-Narasimhan <sandhiya.narasimhan@gmail.com>
2026-04-17 14:22:04 +0530

    Update widget.js

--- blocks/widget/widget.js
+++ blocks/widget/widget.js
@@ -1,11 +1,11 @@
-import icon from '../../icons/lock.png';
+// import icon from '../../icons/lock.png';
 
-const widgetH2 = document.querySelectorAll('.widget h3');
-widgetH2.forEach(ele => {
-  var newImg = document.createElement('img');
-  newImg.classList.add('widget-icon');
-  newImg.src =  `${icon}`;
-  newImg.alt = 'Description';
-  ele.parentNode.insertBefore(newImg, ele);
+// const widgetH2 = document.querySelectorAll('.widget h3');
+// widgetH2.forEach(ele => {
+//   var newImg = document.createElement('img');
+//   newImg.classList.add('widget-icon');
+//   newImg.src =  `${icon}`;
+//   newImg.alt = 'Description';
+//   ele.parentNode.insertBefore(newImg, ele);
   
-})
+// })
